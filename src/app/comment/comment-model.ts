c24625ab86775fe0422d560d79e6069a557e5a8f
KristianKirchev <kris.kirchev@gmail.com>
2023-01-26 18:22:59 +0200

    posts: can create post and topic, can browse posts by topics, can edit post, can comment and react on posts

---
+++ src/app/comment/comment-model.ts
@@ -0,0 +1,8 @@
+export interface CommentModel {
+    id: number;
+    postId: number;
+    text: string;
+    username: string;
+    duration: string;
+  }
+  
--- src/app/post/view-post/view-post.component.ts
+++ src/app/post/view-post/view-post.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { throwError } from 'rxjs';
 import { AuthService } from 'src/app/auth/service/auth.service';
+import { CommentModel } from 'src/app/comment/comment-model';
 import { CommentService } from 'src/app/comment/comment.service';
 import { Modals } from 'src/app/modals';
 import { PostModel } from 'src/app/post/post-model';
@@ -18,6 +19,8 @@ import { ReportService } from 'src/app/report/report.service';
 export class ViewPostComponent implements OnInit {
   post: PostModel;
   commentForm: FormGroup;
+  commentModel: CommentModel;
+  comments: CommentModel[] = [];
   postOwnedByLoggedUser: boolean = false;
 
   constructor(
@@ -26,7 +29,6 @@ export class ViewPostComponent implements OnInit {
     private commentService: CommentService,
     private authService: AuthService,
     private router: Router,
-    private reportService: ReportService,
     private modals: Modals
   ) {
     this.commentForm = new FormGroup({
@@ -48,9 +50,17 @@ export class ViewPostComponent implements OnInit {
       usernameDislikes: [],
       usernameLikes: [],
     };
+    this.commentModel = {
+      id: 0,
+      postId: this.post.id,
+      text: '',
+      username: '',
+      duration: '',
+    };
   }
 
   ngOnInit(): void {
+    this.getCommentsForPost();
     this.activateRoute.params.subscribe((routeParams) => {
       (this.post.id = routeParams['id']),
         this.postService.getPost(this.post.id).subscribe({
@@ -60,7 +70,8 @@ export class ViewPostComponent implements OnInit {
               this.authService.getUserName() == data.userName;
           },
           error: (error) => throwError(() => error),
-        })
+        }),
+        this.getCommentsForPost();
     });
   }
 
@@ -71,9 +82,39 @@ export class ViewPostComponent implements OnInit {
     });
   }
 
+  postComment() {
+    this.commentModel.text = this.commentForm.get('text')!.value;
+    this.commentModel.postId = this.post.id;
+    this.commentService.postComment(this.commentModel).subscribe({
+      next: (data) => this.getCommentsForPost(),
+      error: (error) => throwError(() => error),
+    });
+    this.commentForm.reset();
+  }
+
+  getCommentsForPost() {
+    this.commentService.getAllCommentsForPost(this.post.id).subscribe({
+      next: (data) => (this.comments = data),
+      error: (error) => throwError(() => error),
+    });
+  }
+
   deletePost() {
     this.postService.deletePost(this.post.id).subscribe({
       next: (data) => this.router.navigateByUrl('/'),
+    });
+  }
+
+  isCommentOwnedByLoggedUser(username: string) {
+    return username == this.authService.getUserName();
+  }
+
+  deleteComment(commentId: number) {
+    this.commentService.deleteComment(commentId).subscribe({
+      next: () =>
+        this.commentService.getAllCommentsForPost(this.post.id).subscribe({
+          next: (data) => (this.comments = data),
+        }),
     });
   }
 
